7d07a34e5bd262400255ce5b697d8701b689ef8a
Stefano Scuderi <85753048+stefanoscuderi@users.noreply.github.com>
2023-12-13 19:53:08 -0300

    Add files via upload

---
+++ Ice_Translate2/Ice_Translate/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageDetection;
+using Xamarin.Forms;
+
+namespace Ice_Translate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Traduzir(object sender, EventArgs args)
+        {
+            var detector = new LanguageDetector();
+
+            detector.AddLanguages(new[] { "eng", "por", "ita", "fra", "spa" });
+            var lingua = detector.Detect(txtidioma.Text);
+            if(lingua == "eng")
+            {
+                lingua = "Inglês";
+            }
+            else if(lingua == "spa")
+            {
+                lingua = "Espanhol";
+            }
+            else if(lingua == "por")
+            {
+                lingua = "Português";
+            }
+            else if (lingua == "ita")
+            {
+                lingua = "Italiano";
+            }
+            else if (lingua == "fra")
+            {
+                lingua = "Francês";
+            }
+            txtverificado.Text = lingua;
+        }
+    }
+}
